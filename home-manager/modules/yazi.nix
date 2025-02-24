6b69975b25c0043549f874a0b58535e9f88ddc02
John Doe <johndoe@example.com>
2025-02-24 16:14:40 +0900

    add broot

--- home-manager/modules/yazi.nix
+++ home-manager/modules/yazi.nix
@@ -1,18 +1,20 @@
 { config, lib, pkgs, ... }:
 
 {
-    with pkgs; [
-        ffmpeg
-        p7zip
-        jq
-        poppler
-        fd
-        ripgrep
-        fzf
-        zoxide
-        imagemagick
-        wl-clipboard
-    ];
+    home = {
+        packages = with pkgs; [
+            ffmpeg
+                p7zip
+                jq
+                poppler
+                fd
+                ripgrep
+                fzf
+                zoxide
+                imagemagick
+                wl-clipboard
+        ];
+    };
 
     programs.yazi = {
         enable = true;
