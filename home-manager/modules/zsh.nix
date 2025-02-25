343a1e2055bb0245e5cf88dedeed8122af726c41
John Doe <johndoe@example.com>
2025-02-25 09:58:43 +0900

    fix zsh

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -4,6 +4,7 @@ let
 zshHistory = "${config.xdg.cacheHome}/zsh/history";
 in
 {
+    home.file.".zshrc".source = ./zsh.cfg; 
     programs.zsh = {
         enable = true;
         enableCompletion = true;
@@ -12,6 +13,5 @@ in
         history.path = zshHistory;
 
     };
-    home.file.".zshrc".source = ./zsh.cfg; 
 
 }
