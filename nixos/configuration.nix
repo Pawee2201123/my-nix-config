d84a13076aa0d914c1eb1abf8b3d0f63da660c5f
John Doe <johndoe@example.com>
2025-03-01 21:10:14 +0900

    make zsh faster

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -57,6 +57,7 @@
     };
 
     users.defaultUserShell = pkgs.zsh;
+    programs.zsh.enable = true;
 
 # Define a user account. Don't forget to set a password with ‘passwd’.
     users.users.sisyphus = {
