a0d37670d7cea0ff8f0d071c4fc88706ebe99321
John Doe <johndoe@example.com>
2025-03-04 14:37:09 +0700

    userdirs

--- home-manager/modules/xdg-user-dirs.nix
+++ home-manager/modules/xdg-user-dirs.nix
@@ -1,16 +1,14 @@
 { ... }: 
 {
-    home = {
-        xdg = {
-            userDirs = {
-                enable = true;
-                createDirectories = true;
-                documents = "\$HOME/doc";
-                download = "\$HOME/dl";
-                music = "\$HOME/music";
-                pictures = "\$HOME/pic";
-                videos = "\$HOME/vid";
-            };
+    xdg = {
+        userDirs = {
+            enable = true;
+            createDirectories = true;
+            documents = "\$HOME/doc";
+            download = "\$HOME/dl";
+            music = "\$HOME/music";
+            pictures = "\$HOME/pic";
+            videos = "\$HOME/vid";
         };
     };
 }
