1ed193d3849599ced83887448052294c2aeb5411
John Doe <johndoe@example.com>
2025-03-12 15:19:20 +0700

    rm steam

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -72,20 +72,6 @@
     users.defaultUserShell = pkgs.zsh;
     programs.zsh.enable = true;
 
-    programs.steam = {
-        enable = true;
-        remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-            dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-            localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    };
-    hardware = {
-        graphics = {
-            enable = true;
-            enable32Bit = true;
-        };
-
-# Intel Integrated GPU
-    };
 
 
 # Define a user account. Don't forget to set a password with ‘passwd’.
