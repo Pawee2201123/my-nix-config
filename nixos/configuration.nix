ba10d5db0b7dbb6efa7eb7cf3619482e73db4887
John Doe <johndoe@example.com>
2025-04-12 15:40:42 +0900

    add yubikey relate software

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,13 +1,10 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, inputs ,... }:
 
 {
     imports =
         [ # Include the results of the hardware scan.
-            ./hardware-configuration.nix
+        ./hardware-configuration.nix
             inputs.xremap-flake.nixosModules.default
             ./disko.nix
         ];
@@ -19,19 +16,16 @@
     networking.hostName = "nixos"; # Define your hostname.
 # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-    nix.settings.experimental-features = ["nix-command" "flakes"];
+        nix.settings.experimental-features = ["nix-command" "flakes"];
 
 
-# Configure network proxy if necessary
-# networking.proxy.default = "http://user:password@proxy:port/";
-# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
 # Enable networking
     networking.networkmanager.enable = true;
 
 # Set your time zone.
     time.timeZone = "Asia/Tokyo";
-    #time.timeZone = "Asia/Bangkok";
+#time.timeZone = "Asia/Bangkok";
 
 # Select internationalisation properties.
     i18n.defaultLocale = "en_US.UTF-8";
@@ -64,7 +58,7 @@
         fcitx5.waylandFrontend = true;
         fcitx5.addons = with pkgs; [
             fcitx5-mozc
-            fcitx5-gtk
+                fcitx5-gtk
         ];
     };
 
@@ -85,15 +79,13 @@
 # Allow unfree packages
     nixpkgs.config.allowUnfree = true;
 
-# List packages installed in system profile. To search, run:
-# $ nix search wget
     environment.systemPackages = with pkgs; [
         neovim 
-        home-manager
-        wget
-        git
-        tree
-        tmux
+            home-manager
+            wget
+            git
+            tree
+            tmux
     ];
 
 #fonts
@@ -123,41 +115,31 @@
     hardware.uinput.enable = true;
     users.groups.uinput.members = ["sisyphus"];
     users.groups.input.members = ["sisyphus"];
-# Some programs need SUID wrappers, can be configured further or are
-# started in user sessions.
-# programs.mtr.enable = true;
-# programs.gnupg.agent = {
-#   enable = true;
-#   enableSSHSupport = true;
-# };
-
-# List services that you want to enable:
-
-# Enable the OpenSSH daemon.
+#ssh
     services.openssh = {
         enable = true;
         settings = {
             PermitRootLogin = "yes";            # Allow root login
-            PasswordAuthentication = true;      # Allow password login (or use key auth)
+                PasswordAuthentication = true;      # Allow password login (or use key auth)
         };
     };
 # Open ports in the firewall.
- networking.firewall.allowedTCPPorts = [ 53317 ];
- networking.firewall.allowedUDPPorts = [ 53317 ];
-# Or disable the firewall altogether.
-# networking.firewall.enable = true;
+    networking.firewall.allowedTCPPorts = [ 53317 ];
+    networking.firewall.allowedUDPPorts = [ 53317 ];
 
-# This value determines the NixOS release from which the default
-# settings for stateful data, like file locations and database versions
-# on your system were taken. It‘s perfectly fine and recommended to leave
-# this value at the release version of the first install of this system.
-# Before changing this value read the documentation for this option
-# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
     system.stateVersion = "24.11"; # Did you read the comment?
 
 #For thinkpad
         services.tlp.enable = true;
 # Battery power management
     services.upower.enable = true;
+# yubikey related
+    services.udev.packages = [ pkgs.yubikey-personalization ];
+    services.pcscd.enable = true;
+    security.pam.services = {
+        login.u2fAuth = true;
+        sudo.u2fAuth = true;
+    };
 }
+
 
