d588a0bd52637cbdd545c074adffbe44ea52dd63
John Doe <johndoe@example.com>
2025-04-17 23:28:03 +0900

    resolve zsh-fzf-history conflict with zsh-vi

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -14,24 +14,27 @@
         };
         history.size = 10000;
         plugins = [
-            {
-                name = "zsh-vi-mode";
-                src = "${pkgs.zsh-vi-mode}/share/zsh-vi-mode";
-            }
-            {
-                name = "fzf-tab";
-                src = "${pkgs.zsh-fzf-tab}/share/fzf-tab";
-            }
-            {
-                name = "zsh-fzf-history-search";
-                src = "${pkgs.zsh-fzf-history-search}/share/zsh-fzf-history-search";
-            }
+        {
+            name = "zsh-vi-mode";
+            src = "${pkgs.zsh-vi-mode}/share/zsh-vi-mode";
+        }
+        {
+            name = "fzf-tab";
+            src = "${pkgs.zsh-fzf-tab}/share/fzf-tab";
+        }
+        {
+            name = "zsh-fzf-history-search";
+            src = "${pkgs.zsh-fzf-history-search}/share/zsh-fzf-history-search";
+        }
         ];
+        initExtra = ''
+            zvm_after_init_commands+=(eval "$(fzf --zsh)")
+            '';
         profileExtra = ''
             if [[ $(id -u) -ge 1000 && $(tty) == "/dev/tty1" && -z $(pgrep sway) ]]; then
                 exec sway
                     fi
-        '';
+                    '';
 
     };
 }
