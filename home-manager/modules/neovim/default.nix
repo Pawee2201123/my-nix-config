579e2634cc0fe437a85d53d467b6440d027c56a3
John Doe <johndoe@example.com>
2025-04-28 23:07:57 +0900

    typst lsp

--- home-manager/modules/neovim/default.nix
+++ home-manager/modules/neovim/default.nix
@@ -77,8 +77,8 @@ in {
             arrow-nvim
             which-key-nvim
             blink-cmp
-            typst-vim
-            typst-preview-nvim
+            #typst-vim
+            #typst-preview-nvim
           ];
         };
 
