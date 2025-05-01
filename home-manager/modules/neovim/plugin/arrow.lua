0dd09038b25552ad0f70fc3fa7c8c6c2a9b8f990
John Doe <johndoe@example.com>
2025-05-01 13:30:05 +0900

    remove stupid macro

--- home-manager/modules/neovim/plugin/arrow.lua
+++ home-manager/modules/neovim/plugin/arrow.lua
@@ -4,7 +4,7 @@ end
 
 require('arrow').setup({
   show_icons = false,
-  leader_key = ';', -- Recommended to be a single key
+  leader_key = ' ', -- Recommended to be a single key
   buffer_leader_key = 'm', -- Per Buffer Mappings
 })
 
