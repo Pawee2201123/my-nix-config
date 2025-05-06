3474fba7085420e09e9394a0194495bca34c8f0c
John Doe <johndoe@example.com>
2025-05-06 20:16:47 +0900

    flash.nvim

--- home-manager/modules/neovim/plugin/flash.lua
+++ home-manager/modules/neovim/plugin/flash.lua
@@ -1,1 +1,16 @@
-require("flash").treesitter(opts?)
+require("flash").jump({
+  pattern = "^",
+  label = { after = { 0, 0 } },
+  search = {
+    mode = "search",
+    exclude = {
+      function(win)
+        return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "TelescopeResults"
+      end,
+    },
+  },
+  action = function(match)
+    local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+    picker:set_selection(match.pos[1] - 1)
+  end,
+})
