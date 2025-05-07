44abab63a3ce3e250b302d7c1060e24e4c28afaf
John Doe <johndoe@example.com>
2025-05-07 21:16:16 +0900

    oil.nvim

--- home-manager/modules/neovim/plugin/snacks.lua
+++ home-manager/modules/neovim/plugin/snacks.lua
@@ -17,7 +17,7 @@ require("snacks").setup({
 vim.keymap.set("n", "<c-\\>", function() Snacks.terminal.open() end, { desc = 'Snacks Terminal' })
 vim.keymap.set("n", "<leader>_", function() Snacks.lazygit.open() end, { desc = 'Snacks LazyGit' })
 vim.keymap.set('n', "<leader>sf", function() Snacks.picker.smart() end, { desc = "Smart Find Files" })
-vim.keymap.set("n", "-", function() Snacks.explorer.open() end, { desc = 'Snacks file explorer' })
+-- vim.keymap.set("n", "-", function() Snacks.explorer.open() end, { desc = 'Snacks file explorer' })
 -- find
 vim.keymap.set('n', "<leader>ff", function() Snacks.picker.files() end, { desc = "Find Files" })
 vim.keymap.set('n', "<leader>fg", function() Snacks.picker.git_files() end, { desc = "Find Git Files" })
