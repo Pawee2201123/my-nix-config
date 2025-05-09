015d251febe427a32b7a98f11517c51c930952ab
John Doe <johndoe@example.com>
2025-05-09 22:13:29 +0900

    kanshi

--- home-manager/modules/kanshi.nix
+++ home-manager/modules/kanshi.nix
@@ -4,14 +4,9 @@
         enable = true;
         extraConfig = ''
             output "Dell Inc. DELL S2721DS B8DDQ43" {
-                mode 2560x1440@59.951
-                    position 1280,0
                     alias $HOME
             }
             output "BOE 0x074F Unknown" {
-                mode 1920x1080@60
-                    position 0,0
-                    scale 1.0
                     alias $INTERNAL
             }
 
