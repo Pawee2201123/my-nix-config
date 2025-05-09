1295320cc6dea42add59b5684726ce50414363da
John Doe <johndoe@example.com>
2025-05-09 22:11:40 +0900

    kanshi

--- home-manager/modules/kanshi.nix
+++ home-manager/modules/kanshi.nix
@@ -6,7 +6,6 @@
             output "Dell Inc. DELL S2721DS B8DDQ43" {
                 mode 2560x1440@59.951
                     position 1280,0
-                    scale 1.0
                     alias $HOME
             }
             output "BOE 0x074F Unknown" {
