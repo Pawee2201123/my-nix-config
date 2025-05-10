2a826bf4db17a0c6e872aeeb1bd9561581b1c583
John Doe <johndoe@example.com>
2025-05-10 10:49:47 +0900

    kanshi

--- home-manager/modules/kanshi.nix
+++ home-manager/modules/kanshi.nix
@@ -3,17 +3,33 @@
     services.kanshi = {
         enable = true;
         extraConfig = ''
-            output "Dell Inc. DELL S2721DS B8DDQ43" {
-                    alias $HOME
+            output eDP-1 {
+                mode 1920x1080@60
+                position 0,0
+                alias $internal
             }
-            output "BOE 0x074F Unknown" {
-                    alias $INTERNAL
+
+            output DP-3 {
+                mode 2560x1440@74.971
+                position 1920,0
+                alias $home
+            }
+
+            output HDMI-A-2 {
+                position 1920,0
+                alias $etc
             }
 
             profile home {
-                output $INTERNAL disable
-                output $HOME enable
+                output $internal disable
+                output $home enable
             }
+
+            profile outside {
+                output $internal enable
+                output $etc enable
+            }
+
             '';
     };
 }
